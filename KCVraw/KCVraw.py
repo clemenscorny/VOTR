8e21fc4d2ab47e8c42b44a7c36203f032fc0d853
Clemens Korner <clemens.corny@gmail.com>
2015-09-11 14:46:41 +0200

    KCVraw: refractoring

--- KCVraw/KCVraw.py
+++ KCVraw/KCVraw.py
@@ -7,73 +7,59 @@ import scipy.misc
 class KCVraw(object):
 
     def __init__(self):
-        self.is_first_frame = True
+        # parameters according to the paper --
+        self.padding = 1.0  # extra area surrounding the target
+        #spatial bandwidth (proportional to target)
+        self.output_sigma_factor = 1 / float(16)
+        self.sigma = 0.2  # gaussian kernel bandwidth
+        self.lambda_value = 1e-2  # regularization
+        # linear interpolation factor for adaptation
+        self.interpolation_factor = 0.075
 
     def initialise(self, img, tl, br):
+        self.is_first_frame = True
+
         self.w = br[0]-tl[0]
         self.h = br[1]-tl[1]
 
-        # parameters according to the paper --
-        padding = 1.0  # extra area surrounding the target
-        #spatial bandwidth (proportional to target)
-        output_sigma_factor = 1 / float(16)
-        sigma = 0.2  # gaussian kernel bandwidth
-        lambda_value = 1e-2  # regularization
-        # linear interpolation factor for adaptation
-        interpolation_factor = 0.075
-
-        info = load_info(tl, br)
-        img_files, pos, target_sz, \
-            should_resize_image, ground_truth, video_path = info
+        info = KCVraw.load_info(tl, br)
+        img_files, self.pos, target_sz, \
+            self.should_resize_image, ground_truth, video_path = info
 
         # window size, taking padding into account
-        sz = pylab.floor(target_sz * (1 + padding))
+        self.sz = pylab.floor(target_sz * (1 + self.padding))
 
         # desired output (gaussian shaped), bandwidth proportional to target
         # size
-        output_sigma = pylab.sqrt(pylab.prod(target_sz)) * output_sigma_factor
+        output_sigma = pylab.sqrt(pylab.prod(target_sz)) * \
+            self.output_sigma_factor
 
-        grid_y = pylab.arange(sz[0]) - pylab.floor(sz[0]/2)
-        grid_x = pylab.arange(sz[1]) - pylab.floor(sz[1]/2)
+        grid_y = pylab.arange(self.sz[0]) - pylab.floor(self.sz[0]/2)
+        grid_x = pylab.arange(self.sz[1]) - pylab.floor(self.sz[1]/2)
         #[rs, cs] = ndgrid(grid_x, grid_y)
         rs, cs = pylab.meshgrid(grid_x, grid_y)
         y = pylab.exp(-0.5 / output_sigma**2 * (rs**2 + cs**2))
-        yf = pylab.fft2(y)
+        self.yf = pylab.fft2(y)
         #print("yf.shape ==", yf.shape)
         #print("y.shape ==", y.shape)
 
         # store pre-computed cosine window
-        cos_window = pylab.outer(pylab.hanning(sz[0]),
-                                 pylab.hanning(sz[1]))
+        self.cos_window = pylab.outer(pylab.hanning(self.sz[0]),
+                                      pylab.hanning(self.sz[1]))
 
         #total_time = 0  # to calculate FPS
         #positions = pylab.zeros((len(img_files), 2))  # to calculate precision
 
         global z, response
         z = None
-        self.alphaf = None
         response = None
-
-        self.pos = pos
-        self.should_resize_image = should_resize_image
-        self.sz = sz
-        self.cos_window = cos_window
-        self.yf = yf
+        self.alphaf = None
 
     def process_frame(self, im):
         global z, response
 
-        # parameters according to the paper --
-        padding = 1.0  # extra area surrounding the target
-        #spatial bandwidth (proportional to target)
-        output_sigma_factor = 1 / float(16)
-        sigma = 0.2  # gaussian kernel bandwidth
-        lambda_value = 1e-2  # regularization
-        # linear interpolation factor for adaptation
-        interpolation_factor = 0.075
-
         if len(im.shape) == 3 and im.shape[2] > 1:
-            im = rgb2gray(im)
+            im = KCVraw.rgb2gray(im)
 
         if self.should_resize_image:
             im = scipy.misc.imresize(im, 0.5)
@@ -81,11 +67,11 @@ class KCVraw(object):
         start_time = time.time()
 
         # extract and pre-process subwindow
-        x = get_subwindow(im, self.pos, self.sz, self.cos_window)
+        x = KCVraw.get_subwindow(im, self.pos, self.sz, self.cos_window)
 
         if not self.is_first_frame:
             # calculate response of the classifier at all locations
-            k = dense_gauss_kernel(sigma, x, z)
+            k = KCVraw.dense_gauss_kernel(self.sigma, x, z)
             kf = pylab.fft2(k)
             alphaf_kf = pylab.multiply(self.alphaf, kf)
             response = pylab.real(pylab.ifft2(alphaf_kf))  # Eq. 9
@@ -99,13 +85,13 @@ class KCVraw(object):
 
         # get subwindow at current estimated target position,
         # to train classifer
-        x = get_subwindow(im, self.pos, self.sz, self.cos_window)
+        x = KCVraw.get_subwindow(im, self.pos, self.sz, self.cos_window)
 
         # Kernel Regularized Least-Squares,
         # calculate alphas (in Fourier domain)
-        k = dense_gauss_kernel(sigma, x)
+        k = KCVraw.dense_gauss_kernel(self.sigma, x)
         # Eq. 7
-        new_alphaf = pylab.divide(self.yf, (pylab.fft2(k) + lambda_value))
+        new_alphaf = pylab.divide(self.yf, (pylab.fft2(k) + self.lambda_value))
         new_z = x
 
         if self.is_first_frame:
@@ -127,131 +113,131 @@ class KCVraw(object):
 
         self.is_is_first_frame = False
 
+    @staticmethod
+    def load_info(tl, br):
+        x = tl[0]
+        y = tl[1]
+        w = br[0]-tl[0]
+        h = br[1]-tl[1]
 
-def load_info(tl, br):
-    x = tl[0]
-    y = tl[1]
-    w = br[0]-tl[0]
-    h = br[1]-tl[1]
+        # set initial position and size
+        first_ground_truth = [x, y, w, h]
+        # target_sz contains height, width
+        target_sz = pylab.array([first_ground_truth[3], first_ground_truth[2]])
+        # pos contains y, x center
+        pos = [first_ground_truth[1], first_ground_truth[0]] \
+            + pylab.floor(target_sz / 2)
 
-    # set initial position and size
-    first_ground_truth = [x, y, w, h]
-    # target_sz contains height, width
-    target_sz = pylab.array([first_ground_truth[3], first_ground_truth[2]])
-    # pos contains y, x center
-    pos = [first_ground_truth[1], first_ground_truth[0]] \
-        + pylab.floor(target_sz / 2)
+        # if the target is too large, use a lower resolution
+        # no need for so much detail
+        if pylab.sqrt(pylab.prod(target_sz)) >= 100:
+            pos = pylab.floor(pos / 2)
+            target_sz = pylab.floor(target_sz / 2)
+            resize_image = True
+        else:
+            resize_image = False
 
-    # if the target is too large, use a lower resolution
-    # no need for so much detail
-    if pylab.sqrt(pylab.prod(target_sz)) >= 100:
-        pos = pylab.floor(pos / 2)
-        target_sz = pylab.floor(target_sz / 2)
-        resize_image = True
-    else:
-        resize_image = False
+        ret = [None, pos, target_sz, resize_image, None, None]
+        return ret
 
-    ret = [None, pos, target_sz, resize_image, None, None]
-    return ret
+    @staticmethod
+    def rgb2gray(rgb_image):
+        "Based on http://stackoverflow.com/questions/12201577"
+        # [0.299, 0.587, 0.144] normalized gives [0.29, 0.57, 0.14]
+        return pylab.dot(rgb_image[:, :, :3], [0.29, 0.57, 0.14])
 
+    @staticmethod
+    def get_subwindow(im, pos, sz, cos_window):
+        """
+        Obtain sub-window from image, with replication-padding.
+        Returns sub-window of image IM centered at POS ([y, x] coordinates),
+        with size SZ ([height, width]). If any pixels are outside of the image,
+        they will replicate the values at the borders.
 
-def rgb2gray(rgb_image):
-    "Based on http://stackoverflow.com/questions/12201577"
-    # [0.299, 0.587, 0.144] normalized gives [0.29, 0.57, 0.14]
-    return pylab.dot(rgb_image[:, :, :3], [0.29, 0.57, 0.14])
+        The subwindow is also normalized to range -0.5 .. 0.5, and the given
+        cosine window COS_WINDOW is applied
+        (though this part could be omitted to make the function more general).
+        """
 
+        if pylab.isscalar(sz):  # square sub-window
+            sz = [sz, sz]
 
-def get_subwindow(im, pos, sz, cos_window):
-    """
-    Obtain sub-window from image, with replication-padding.
-    Returns sub-window of image IM centered at POS ([y, x] coordinates),
-    with size SZ ([height, width]). If any pixels are outside of the image,
-    they will replicate the values at the borders.
+        ys = pylab.floor(pos[0]) \
+            + pylab.arange(sz[0], dtype=int) - pylab.floor(sz[0]/2)
+        xs = pylab.floor(pos[1]) \
+            + pylab.arange(sz[1], dtype=int) - pylab.floor(sz[1]/2)
 
-    The subwindow is also normalized to range -0.5 .. 0.5, and the given
-    cosine window COS_WINDOW is applied
-    (though this part could be omitted to make the function more general).
-    """
+        ys = ys.astype(int)
+        xs = xs.astype(int)
 
-    if pylab.isscalar(sz):  # square sub-window
-        sz = [sz, sz]
+        # check for out-of-bounds coordinates,
+        # and set them to the values at the borders
+        ys[ys < 0] = 0
+        ys[ys >= im.shape[0]] = im.shape[0] - 1
 
-    ys = pylab.floor(pos[0]) \
-        + pylab.arange(sz[0], dtype=int) - pylab.floor(sz[0]/2)
-    xs = pylab.floor(pos[1]) \
-        + pylab.arange(sz[1], dtype=int) - pylab.floor(sz[1]/2)
+        xs[xs < 0] = 0
+        xs[xs >= im.shape[1]] = im.shape[1] - 1
+        #zs = range(im.shape[2])
 
-    ys = ys.astype(int)
-    xs = xs.astype(int)
+        # extract image
+        #out = im[pylab.ix_(ys, xs, zs)]
+        out = im[pylab.ix_(ys, xs)]
 
-    # check for out-of-bounds coordinates,
-    # and set them to the values at the borders
-    ys[ys < 0] = 0
-    ys[ys >= im.shape[0]] = im.shape[0] - 1
+        #pre-process window --
+        # normalize to range -0.5 .. 0.5
+        # pixels are already in range 0 to 1
+        out = out.astype(pylab.float64) - 0.5
 
-    xs[xs < 0] = 0
-    xs[xs >= im.shape[1]] = im.shape[1] - 1
-    #zs = range(im.shape[2])
+        # apply cosine window
+        out = pylab.multiply(cos_window, out)
 
-    # extract image
-    #out = im[pylab.ix_(ys, xs, zs)]
-    out = im[pylab.ix_(ys, xs)]
+        return out
 
-    #pre-process window --
-    # normalize to range -0.5 .. 0.5
-    # pixels are already in range 0 to 1
-    out = out.astype(pylab.float64) - 0.5
+    @staticmethod
+    def dense_gauss_kernel(sigma, x, y=None):
+        """
+        Gaussian Kernel with dense sampling.
+        Evaluates a gaussian kernel with bandwidth SIGMA for all displacements
+        between input images X and Y, which must both be MxN. They must also
+        be periodic (ie., pre-processed with a cosine window). The result is
+        an MxN map of responses.
 
-    # apply cosine window
-    out = pylab.multiply(cos_window, out)
+        If X and Y are the same, ommit the third parameter to re-use some
+        values, which is faster.
+        """
 
-    return out
+        xf = pylab.fft2(x)  # x in Fourier domain
+        x_flat = x.flatten()
+        xx = pylab.dot(x_flat.transpose(), x_flat)  # squared norm of x
 
+        if y is not None:
+            # general case, x and y are different
+            yf = pylab.fft2(y)
+            y_flat = y.flatten()
+            yy = pylab.dot(y_flat.transpose(), y_flat)
+        else:
+            # auto-correlation of x, avoid repeating a few operations
+            yf = xf
+            yy = xx
 
-def dense_gauss_kernel(sigma, x, y=None):
-    """
-    Gaussian Kernel with dense sampling.
-    Evaluates a gaussian kernel with bandwidth SIGMA for all displacements
-    between input images X and Y, which must both be MxN. They must also
-    be periodic (ie., pre-processed with a cosine window). The result is
-    an MxN map of responses.
+        # cross-correlation term in Fourier domain
+        xyf = pylab.multiply(xf, pylab.conj(yf))
 
-    If X and Y are the same, ommit the third parameter to re-use some
-    values, which is faster.
-    """
+        # to spatial domain
+        xyf_ifft = pylab.ifft2(xyf)
+        #xy_complex = circshift(xyf_ifft, floor(x.shape/2))
+        row_shift, col_shift = pylab.floor(pylab.array(x.shape)/2).astype(int)
+        xy_complex = pylab.roll(xyf_ifft, row_shift, axis=0)
+        xy_complex = pylab.roll(xy_complex, col_shift, axis=1)
+        xy = pylab.real(xy_complex)
 
-    xf = pylab.fft2(x)  # x in Fourier domain
-    x_flat = x.flatten()
-    xx = pylab.dot(x_flat.transpose(), x_flat)  # squared norm of x
+        # calculate gaussian response for all positions
+        scaling = -1 / (sigma**2)
+        xx_yy = xx + yy
+        xx_yy_2xy = xx_yy - 2 * xy
+        k = pylab.exp(scaling * pylab.maximum(0, xx_yy_2xy / x.size))
 
-    if y is not None:
-        # general case, x and y are different
-        yf = pylab.fft2(y)
-        y_flat = y.flatten()
-        yy = pylab.dot(y_flat.transpose(), y_flat)
-    else:
-        # auto-correlation of x, avoid repeating a few operations
-        yf = xf
-        yy = xx
+        #print("dense_gauss_kernel x.shape ==", x.shape)
+        #print("dense_gauss_kernel k.shape ==", k.shape)
 
-    # cross-correlation term in Fourier domain
-    xyf = pylab.multiply(xf, pylab.conj(yf))
-
-    # to spatial domain
-    xyf_ifft = pylab.ifft2(xyf)
-    #xy_complex = circshift(xyf_ifft, floor(x.shape/2))
-    row_shift, col_shift = pylab.floor(pylab.array(x.shape)/2).astype(int)
-    xy_complex = pylab.roll(xyf_ifft, row_shift, axis=0)
-    xy_complex = pylab.roll(xy_complex, col_shift, axis=1)
-    xy = pylab.real(xy_complex)
-
-    # calculate gaussian response for all positions
-    scaling = -1 / (sigma**2)
-    xx_yy = xx + yy
-    xx_yy_2xy = xx_yy - 2 * xy
-    k = pylab.exp(scaling * pylab.maximum(0, xx_yy_2xy / x.size))
-
-    #print("dense_gauss_kernel x.shape ==", x.shape)
-    #print("dense_gauss_kernel k.shape ==", k.shape)
-
-    return k
+        return k
